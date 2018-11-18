81f0e8378294b16031bdf09593dfe2a121c3699b
Sven Berger <sven@berger-lembeck.de>
2018-11-18 17:14:51 +0100

    MOSQUITTO: Entrypoint angepasst

--- mosquitto/amd64/scripte/entrypoint.sh
+++ mosquitto/amd64/scripte/entrypoint.sh
@@ -5,4 +5,12 @@ set -e
 echo "Benutzername: " $USER
 echo "Password:     " $PASSWD
 
+echo "Passwort erstellen"
+touch /tmp/passwd
+mosquitto_passwd -b /tmp/passwd '$USER' '$PASSWD'
+cat /tmp/passwd     
+cat /tmp/passwd > /mosquitto/config/passwd
+
+echo "Passwort erstellt, start Mosquitto"
+
 exec "$@"
